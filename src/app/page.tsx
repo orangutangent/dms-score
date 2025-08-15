85326e98e5e341d90e9c05caf588a61f42c20c24
Denis <dkartsefff@gmail.com>
2025-08-15 18:31:20 +0700

    updated index page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,12 @@ export default function Home() {
       <div className="flex-grow flex items-center justify-center p-8 min-h-screen">
         <div className="w-full max-w-5xl text-center">
           <h1 className="text-2xl font-bold  mb-4">{t("mainTitle")}</h1>
-          <p className="mt-6 text-gray-600">{t("description1")}</p>
-          <p className="mt-6 text-gray-600">{t("description2")}</p>
-          <p className="mt-6 text-gray-600">{t("invitationText")}</p>
-          <div className="mt-12 flex flex-col lg:flex-row gap-6 justify-center">
+          <p className="mt-4 text-gray-600">{t("description1")}</p>
+          <p className="mt-4 text-gray-600">{t("description2")}</p>
+          <p className="mt-4 text-gray-600">{t("description3")}</p>
+          <p className="mt-4 text-gray-600">{t("description4")}</p>
+          <p className="mt-4 text-gray-600">{t("invitationText")}</p>
+          <div className="mt-6 flex flex-col lg:flex-row gap-6 justify-center">
             <Link href="/govermentssurvey">
               <Button>{t("startGovButton")}</Button>
             </Link>
@@ -23,40 +25,40 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="p-8">
+      <div className="p-4">
         <div className="w-full max-w-5xl mx-auto">
-            <p className="mt-6 text-gray-600">
-              {t("projectInfo_preLink")}
-              <Link
-                href={t("projectInfo_linkUrl")}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-600 hover:underline"
-              >
-                {t("projectInfo_linkText")}
-              </Link>
-              {t("projectInfo_postLink")}
-            </p>
-            <p className="mt-6 text-gray-600">
-              {t("roundtableInfoText")}
-            </p>
+          <p className="mt-6 text-gray-600">
+            {t("projectInfo_preLink")}
             <Link
-                href={t("roundtableInfoLinkUrl")}
-                target="_blank"
-                rel="noopener noreferrer"
+              href={t("projectInfo_linkUrl")}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:underline"
+            >
+              {t("projectInfo_linkText")}
+            </Link>
+            {t("projectInfo_postLink")}
+          </p>
+          <p className="mt-6 text-gray-600">{t("roundtableInfoText")}</p>
+          <Link
+            href={t("roundtableInfoLinkUrl")}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-600 hover:underline"
+          >
+            {t("roundtableInfoLinkUrl")}
+          </Link>
+          <div className="mt-6 space-y-2 text-gray-500">
+            <p>
+              {t("contactInfo_preLink")}
+              <a
+                href={`mailto:${t("contactInfo_email")}`}
                 className="text-blue-600 hover:underline"
               >
-                {t("roundtableInfoLinkUrl")}
-            </Link>
-            <div className="mt-6 space-y-2 text-gray-500">
-              <p>{t("timeToComplete")}</p>
-              <p>
-                {t("contactInfo_preLink")}
-                <a href={`mailto:${t("contactInfo_email")}`} className="text-blue-600 hover:underline">
-                  {t("contactInfo_email")}
-                </a>
-              </p>
-            </div>
+                {t("contactInfo_email")}
+              </a>
+            </p>
+          </div>
         </div>
       </div>
     </main>

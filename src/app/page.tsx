3486d6f56699b250b7c1b0d419a622a541f682fd
Denis <dkartsefff@gmail.com>
2025-08-15 18:10:24 +0700

    updated styling index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,52 +6,57 @@ export default function Home() {
   const t = useTranslations("IndexPage");
 
   return (
-    <main className="flex-1 flex items-center justify-center p-8">
-      <div className="w-full max-w-5xl ">
-        <h1 className="text-2xl font-bold  mb-4">{t("mainTitle")}</h1>
-        <p className="mt-6 text-gray-600">{t("description1")}</p>
-        <p className="mt-6 text-gray-600">{t("description2")}</p>
-        <p className="mt-6 text-gray-600">{t("invitationText")}</p>
-        <div className="mt-12 flex flex-col lg:flex-row gap-6">
-          <Link href="/govermentssurvey">
-            <Button>{t("startGovButton")}</Button>
-          </Link>
-          <Link href="/digitalmaturity">
-            <Button>{t("startMspButton")}</Button>
-          </Link>
+    <main className="flex-1 flex flex-col">
+      <div className="flex-grow flex items-center justify-center p-8 min-h-screen">
+        <div className="w-full max-w-5xl text-center">
+          <h1 className="text-2xl font-bold  mb-4">{t("mainTitle")}</h1>
+          <p className="mt-6 text-gray-600">{t("description1")}</p>
+          <p className="mt-6 text-gray-600">{t("description2")}</p>
+          <p className="mt-6 text-gray-600">{t("invitationText")}</p>
+          <div className="mt-12 flex flex-col lg:flex-row gap-6 justify-center">
+            <Link href="/govermentssurvey">
+              <Button>{t("startGovButton")}</Button>
+            </Link>
+            <Link href="/digitalmaturity">
+              <Button>{t("startMspButton")}</Button>
+            </Link>
+          </div>
         </div>
-        <p className="mt-6 text-gray-600">
-          {t("projectInfo_preLink")}
-          <Link
-            href={t("projectInfo_linkUrl")}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-600 hover:underline"
-          >
-            {t("projectInfo_linkText")}
-          </Link>
-          {t("projectInfo_postLink")}
-        </p>
-        <p className="mt-6 text-gray-600">{t("roundtableInfoText")}</p>
-        <Link
-          href={t("roundtableInfoLinkUrl")}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-600 hover:underline"
-        >
-          {t("roundtableInfoLinkUrl")}
-        </Link>
-        <div className="mt-6 space-y-2 text-gray-500">
-          <p>{t("timeToComplete")}</p>
-          <p>
-            {t("contactInfo_preLink")}
-            <a
-              href={`mailto:${t("contactInfo_email")}`}
-              className="text-blue-600 hover:underline"
-            >
-              {t("contactInfo_email")}
-            </a>
-          </p>
+      </div>
+      <div className="p-8">
+        <div className="w-full max-w-5xl mx-auto">
+            <p className="mt-6 text-gray-600">
+              {t("projectInfo_preLink")}
+              <Link
+                href={t("projectInfo_linkUrl")}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 hover:underline"
+              >
+                {t("projectInfo_linkText")}
+              </Link>
+              {t("projectInfo_postLink")}
+            </p>
+            <p className="mt-6 text-gray-600">
+              {t("roundtableInfoText")}
+            </p>
+            <Link
+                href={t("roundtableInfoLinkUrl")}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 hover:underline"
+              >
+                {t("roundtableInfoLinkUrl")}
+            </Link>
+            <div className="mt-6 space-y-2 text-gray-500">
+              <p>{t("timeToComplete")}</p>
+              <p>
+                {t("contactInfo_preLink")}
+                <a href={`mailto:${t("contactInfo_email")}`} className="text-blue-600 hover:underline">
+                  {t("contactInfo_email")}
+                </a>
+              </p>
+            </div>
         </div>
       </div>
     </main>
